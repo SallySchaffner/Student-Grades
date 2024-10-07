d4213f82e7b40c4eb73980deddf7639840a500fa
Sally Schaffner <schaffners@vpcc.edu>
2024-10-07 15:21:36 -0400

    Need to debug getStudent data

--- Student Grades.cpp
+++ Student Grades.cpp
@@ -1,11 +1,92 @@
-// Student Grades.cpp : This project will contain two branches, 
-// one with the parallel arrays for the frist branch. The second branch will use
-// structs.
-//
+// Student Grades.cpp : The parallel arrays version
+// Put what the program does 
 
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+const int MAX_STUDENTS = 35;
+const int MAX_SCORES = 5;
+const string dataFile = "grades.txt";
+
+//Reads data from the input file, expects the input file to be available. 
+//Returns the actual number of records on the file.
+//Places the data in predeclared storage in memory. 
+//Only allows MAX_STUDENTS to be read from the file.  
+int getStudentData(string names[], double grades[][MAX_SCORES], int maxRecords);
+
+//Functions receives preloaded scores for each student, location for 
+//calculated averages.
+//Fills in the calculated averages for each student
+void calcAverages(const double scores[][MAX_SCORES], double averages[], int numRec);
+
+// Takes a score in the range of 0 - 100 and returns a letter grade (A, B, C, D, F)
+char getLetterGrade(double score);
+
+//Write formatted report displaying the name, average, letter grade for each student. 
+//Report also calculate and displays the class average
+void writeReport(string names[], double averages[], char letterG[], int numRec);
 
 int main()
 {
-    std::cout << "Student Grades\n";
+	string studentNames[MAX_STUDENTS];
+	double grades[MAX_STUDENTS][MAX_SCORES];
+	char letterGrades[MAX_STUDENTS];
+	double avgScore[MAX_STUDENTS];
+
+	int numRecords;
+
+	numRecords = getStudentData(studentNames, grades, MAX_STUDENTS);
+	if (numRecords == -1)
+	{
+		cout << "Too many records on file" << endl;
+		exit;
+	}
+
+	// For debugging
+	cout << "Data from file\n";
+	for (int row = 0; row < numRecords; row++)
+	{
+		cout << studentNames[row] << " ";
+		for (int col = 0; col < MAX_SCORES; col++)
+			cout << grades[row][col] << " ";
+		cout << endl;
+	}
+}
+
+int getStudentData(string names[], double grades[][MAX_SCORES], int maxRecords)
+{
+	ifstream inFile(dataFile);
+	int rec = 0;
+
+	if (!inFile)
+	{
+		cout << "Could not open " << dataFile << endl;
+		exit;
+	}
+
+	while (inFile >> names[rec])
+	{
+		if (rec < MAX_STUDENTS)
+		{
+			for (int j = 0; j < MAX_SCORES; j++)
+				inFile >> grades[rec][j];
+			rec++;
+		}
+		else
+			return -1;
+	}
+		
+	inFile.close();
+
+	return rec;
+			
+	}
+
+	
+
+
 }
