5c0c2eca87f89657abac85a9ddc92a897efe7c64
Sally Schaffner <schaffners@vpcc.edu>
2024-10-07 14:09:47 -0400

    Add project files.

---
+++ Student Grades.cpp
@@ -0,0 +1,11 @@
+// Student Grades.cpp : This project will contain two branches, 
+// one with the parallel arrays for the frist branch. The second branch will use
+// structs.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Student Grades\n";
+}
